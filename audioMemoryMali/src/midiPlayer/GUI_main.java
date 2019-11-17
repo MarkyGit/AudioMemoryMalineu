0da2dd10814f91ac41d9aa49cd32742a1c660dfd
mz <mz@dhclient-91-190-8-141.flashcable.ch>
2019-11-17 17:37:54 +0100

    Mark erster neuer commit

---
+++ audioMemoryMali/src/midiPlayer/GUI_main.java
@@ -0,0 +1,55 @@
+package midiPlayer;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class GUI_main extends Application {
+
+	private Stage primaryStage;
+	private BorderPane mainLayout;
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Sound Memory");
+		showMainView();
+		showButtons();
+	}
+
+	private void showMainView() {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(GUI_main.class.getResource("GUI.fxml"));
+		try {
+			mainLayout = loader.load();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+
+	private void showButtons() throws IOException {
+
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("Buttons.fxml"));
+		GridPane buttons = loader.load();
+		mainLayout.setCenter(buttons);
+
+	}
+
+	public static void main(String[] args) {
+
+		launch(args);
+
+	}
+
+}
---
+++ audioMemoryMali/src/midiPlayer/MiniMiniMusikProg.java
@@ -0,0 +1,51 @@
+package midiPlayer;
+
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
+public class MiniMiniMusikProg {
+
+	public static void main(String[] args) {
+		MiniMiniMusikProg mini = new MiniMiniMusikProg();
+		mini.spielen();
+	}
+
+	public void spielen() {
+		try {
+
+			Sequencer player = MidiSystem.getSequencer();
+			player.open();
+
+			Sequence seq = new Sequence(Sequence.PPQ, 4);
+
+			Track track = seq.createTrack();
+
+			ShortMessage a = new ShortMessage();
+
+			a.setMessage(144, 1, 44, 110);
+			// a.setMessage(192, 1, 102, 0);// Möglichkeit, zum Instrumenten-Wechsel
+			MidiEvent noteOn = new MidiEvent(a, 1);
+			track.add(noteOn);
+
+			ShortMessage b = new ShortMessage();
+			b.setMessage(128, 1, 44, 127);
+			MidiEvent noteOff = new MidiEvent(b, 16);
+			track.add(noteOff);
+
+			player.setSequence(seq);
+			player.start();
+			Thread.sleep(5000);
+			player.close();
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+
+		}
+
+	}
+
+}
